3272361a21fed998b179dbab03dfdf6bdf8b8ef2
Lê Kiên <ltk182001@gmail.com>
2021-11-18 15:44:21 +0700

    State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.scss';
+import { useState } from 'react';
 
+
+const orders = [10, 20, 30]
 function App() {
+  const [counter, setCounter] = useState(() => {
+    const total = orders.reduce((total, cur) => total + cur);
+    console.log(total)
+    return total;
+  })
+
+  const Increase = () => {
+    setCounter(prevState => prevState + 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <button onClick={Increase}>Tăng</button>
     </div>
   );
 }
